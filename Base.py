22ed47b5932e625b0b3cccdf905bca35bb475825
evangandy9600 <96596311+evangandy9600@users.noreply.github.com>
2022-02-01 00:07:31 -0600

    Update Base.py

--- Base.py
+++ Base.py
@@ -1,8 +1,8 @@
 # Evan's GPA Calculator
-# Formula: (Grade point value * credits) / (total number of credits)
 
 # Dictionary
 user_dict = { # Change this dictionary to match unweighted 4.0 GPA scale
+    "A+": 4.0,
     "A": 4.0, # A is worth 4 points always
     "A-": 3.67,
     "B+": 3.34,
@@ -23,73 +23,78 @@ weight_dict = {
 "REG": 0.0
 }
 
-# Total GPA Arrays
-credit_values = []
+credit_values = [] # Total GPA Arrays
 point_values = []
 weighted_point_values = []
+data_save = []
 
-def GPA_calculator(credit_val, point_val, w_point_val, type_gpa):
+def GPA_calculator(credit_val, point_val, w_point_val, type_gpa, sem):
     x = sum(credit_val)
     y = sum(point_val)
     z = sum(w_point_val)
     uw_gpa = y / x # unweighted calculation
     w_gpa = z / x # weighted calculation
+
     if type_gpa == "Semester":
         credit_values.append(x)
         point_values.append(y)
         weighted_point_values.append(z)
-        print(f"\n{type_gpa} Unweighted: {uw_gpa}")
-        print(f"{type_gpa} Weighted: {w_gpa}\n")
+        print(f"\n{type_gpa} ({sem}) Unweighted: {uw_gpa}")
+        print(f"{type_gpa} ({sem}) Weighted: {w_gpa}\n")
+
     elif type_gpa == "Final":
         print(f"Final GPA Unweighted: {round(uw_gpa, 2)}") # Print GPA
         print(f"Final GPA Weighted: {round(w_gpa, 2)}") # Print GPA
 
-# Retrives Grade Information
 def get_grade():
     enter = 0
-    sem_class = int(input("Total semesters taken: "))
+    sem_class = int(input("Total Semesters Taken: "))
+    credit_type = input("Manual Credit Entry? (Y/N): ")
 
     while sem_class > enter: # Semester
         enter += 1
         current_sem = enter # keeps semester updates
 
-        # Semester Array
-        sem_credit_values = []
+        sem_credit_values = [] # Semester Array
         sem_point_values = []
         sem_weighted_point_values = []
+        sem_data_save = []
 
         def class_grade(): # Gets each classes grade
             enter2 = 0
-            num_class = int(input(f"Total classes in semester ({current_sem}): "))
+            num_class = int(input(f"Number of GPA Classes in Semester ({current_sem}): "))
 
-            # Get class info
             while num_class > enter2:
                 enter2 += 1
-                # Student input
+                credits = 1
+
                 name = input(f"\nClass: ")
                 grade_weight = input(f"\tInput (Reg), (Hon) or (AP): ")
-                credits = int(input(f"\t{grade_weight.upper()} {name.capitalize()} credits: "))
+                if credit_type.upper() == "Y":
+                    credits = int(input(f"\t{grade_weight.upper()} {name.capitalize()} credits: "))
+
                 grade = input(f"\t{grade_weight.upper()} {name.capitalize()} letter grade: ")
-                print(f"\t{grade_weight.upper()} {name.capitalize()} -> Credits: ({credits}) Letter Grade: ({grade.upper()})")
                 
-                # Credits and class difficulty calculations
                 sem_credit_values.append(credits) # save credits in array
                 get_grade_weight = float(weight_dict.get(grade_weight.upper())) # save class difficulty in var
+                print(f"\t{grade_weight.upper()} {name.capitalize()} -> Credits: ({credits}) Letter Grade: ({grade.upper()})")
 
-                # Dictionary based calculations
-                def course_value(dict): # Parent function
+                def course_value(dict):
                     get_grade_point = float(dict.get(grade.upper()))
                     uw = credits * (get_grade_point) # Unweighted grade point calculation
-                    w = credits * (get_grade_point + get_grade_weight) # Weighted grade point calculation
+                    w_check = (get_grade_point + get_grade_weight)
+                    w = credits * w_check # Weighted grade point calculation
                     sem_point_values.append(uw)
                     sem_weighted_point_values.append(w)
 
-
+                    grade_save = [current_sem, grade_weight.upper(), name.capitalize(), credits, grade.upper(), w_check]
+                    # print(f"\n{grade_save}")
+                    sem_data_save.append(grade_save)
+                    # print(f"\n{sem_data_save}")
                 course_value(user_dict) # Using user_dict to calculate GPA
-
+        data_save.append(sem_data_save)
         class_grade()
-
-        # Semester: Retrive totals from credit, grade/point value arrays
-        GPA_calculator(credit_val = sem_credit_values, point_val = sem_point_values, w_point_val = sem_weighted_point_values, type_gpa = "Semester")
+        GPA_calculator(credit_val = sem_credit_values, point_val = sem_point_values, w_point_val = sem_weighted_point_values, type_gpa = "Semester", sem = current_sem)
 get_grade() # runs GPA calculator
-GPA_calculator(credit_val = credit_values, point_val = point_values, w_point_val = weighted_point_values, type_gpa = "Final")
+GPA_calculator(credit_val = credit_values, point_val = point_values, w_point_val = weighted_point_values, type_gpa = "Final", sem = 0)
+print(f"\nCopy the follow to save your data:\n\n{data_save}")
